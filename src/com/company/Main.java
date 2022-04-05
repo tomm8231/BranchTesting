15154ea236f64f99743072ddbb4da267237fc828
tomm8231 <tomm8231@stud.kea.dk>
2022-04-05 09:19:06 +0200

    main method creates a Dog and Cat object and make them execute makeSound().

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Cat cat1 = new Cat();
+
+        dog1.makeSound();
+        cat1.makeSound();
     }
 }
